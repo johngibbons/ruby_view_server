4478cae17391d95b80430e6eab21180d193d18dc
johngibbons <johngibbons10@gmail.com>
2014-09-22 21:55:50 -0600

    Using a Layout to Add Content To All Pages

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,14 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+name="John"
+toShow = ERB.new "My name is <%= name %>"
+puts toShow.result(binding)
+
+result = x*24
+equationOut = ERB.new "Using the earlier result, the value of x times 42 is <%= result %>"
+puts equationOut.result(binding)
+
+math = ERB.new "We can also do equations inside of the tages like this.  X divided by 10 equals <%= x/10 %>"
+puts math.result(binding)
